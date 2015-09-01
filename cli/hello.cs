d8917f2a82f52d29e1b82b16cd54dfe97a757f33
kira1928 <kira1928@gmail.com>
2015-09-01 23:24:55 +0800

    init hand succeed

--- cli/hello.cs
+++ cli/hello.cs
@@ -7,12 +7,14 @@ using System.Threading;
  
 public class HelloWorld
 {
-    static public void Main ()
-    {
-		PomeloClient pclient = new PomeloClient ();
-        Console.WriteLine ("Hello Mono World");
+	static PomeloClient pclient = new PomeloClient ();
+
+	static public void Main ()
+	{
+		Console.WriteLine ("Hello Mono World");
 		string host = "hacklu.com";//(www.xxx.com/127.0.0.1/::1/localhost etc.)
 		int port = 7778;
+		PomeloClient pclient = HelloWorld.pclient;
 		
 		//listen on network state changed event
 		pclient.NetWorkStateChangedEvent += (state) =>
@@ -29,28 +31,101 @@ public class HelloWorld
 				Console.WriteLine ("connect response msg: ");
 				//process handshake call back data
 				pclient.on ("onEnterRoom", (responseJson) => {
-					Console.WriteLine ("enter room");
+					Log ("enter room");
 				});
 				pclient.on ("onRoomMessage", (responseJson) => {
-					Console.WriteLine ("received roomMessage: " + responseJson["roomMessage"]);
+					Log ("received roomMessage: " + responseJson["roomMessage"]);
 				});
 				pclient.on ("onLeaveRoom", (responseJson) => {
-					Console.WriteLine ("leave room");
+					Log ("leave room");
 				});
 				pclient.on ("disconnect", (responseJson) => {
-					Console.WriteLine ("disconnect");
+					Log ("disconnect");
 				});
+				pclient.on ("serverRpc", (responseJson) => {
+					int rpcId = Convert.ToInt32(responseJson["_rpcId"]);
+					string cmd = (string)responseJson["_cmd"];
+					rpcHandler(rpcId, cmd, responseJson);
+				});
+				pclient.on ("initHand", onInitHand);
 				JsonObject msg = new JsonObject ();
 				msg ["uid"] = "kira";
-				pclient.request ("connector.gameHandler.roomTest", msg, OnQuery);
+				pclient.request ("connector.gameHandler.roomTest", msg, (JsonObject result) => {
+					Log ("entry result code : " + result ["code"]);
+				});
 			});
 		});
-		
 		Thread.Sleep(10000);
-    }
-	
-	static void OnQuery (JsonObject result)
-	{
-		Console.WriteLine ("entry result code : " + result ["code"]);
+	}
+
+	static void Log(string msg) {
+		// Console.WriteLine(msg);
+	}
+
+	static void DevLog(string msg) {
+		Console.WriteLine(msg);
+	}
+
+	static void onInitHand(JsonObject responseJson) {
+		JsonArray cards = (JsonArray)responseJson["cards"];
+		int count = cards.Count;
+		for( int i = 0; i < count; ++i ) {
+			DevLog(""+Convert.ToInt32(cards[i]));
+		}
+		DevLog("initHand: " + count);
+	}
+
+	// 每个正在通信中的rpc通道都保存在这里
+	// 第一个参数是rpcId的数值
+	// 从设计上来说，rpc通信结束后应该从这个Dict中清理掉
+	static Dictionary<int, IEnumerator> rpcHandlers = new Dictionary<int, IEnumerator>();
+
+	// 本来应该是保存在各个Handler里的，也就是应该扩展IEnumerator，不过我最近赶时间，先这样吧！
+	static Dictionary<int, JsonObject> rpcResponse = new Dictionary<int, JsonObject>();
+
+	static void rpcHandler(int rpcId, string cmd, JsonObject dataJson) {
+		DevLog("" + rpcId + ", " + cmd);
+		// 如果是正在处理中的rpc通信，让他接着处理
+		if (rpcHandlers.ContainsKey(rpcId)) {
+			rpcResponse[rpcId] = dataJson;
+
+			// 如果全部执行完了，就把这个handler移除掉
+			if (!rpcHandlers[rpcId].MoveNext()) {
+				rpcHandlers.Remove(rpcId);
+			}
+			return;
+		}
+
+		// 如果是初次到来的rpc，根据cmd寻找对应的处理函数
+
+		// 这次暂时省略寻找cmd对应函数的步骤，直接用initHand函数来接待。
+		IEnumerator ienum = initHand(rpcId, dataJson).GetEnumerator();
+		bool result = ienum.MoveNext();
+
+		// 如果还有后续，就先存起来，等以后再调用
+		if (result) {
+			rpcHandlers[rpcId] = ienum;
+		}
+	}
+
+	static IEnumerable<int> initHand(int rpcId, JsonObject dataJson) {
+		JsonArray cards = (JsonArray)dataJson["cards"];
+		int count = cards.Count;
+		for( int i = 0; i < count; ++i ) {
+			DevLog(""+Convert.ToInt32(cards[i]));
+		}
+		DevLog("initHand: " + count);
+		JsonObject msg = new JsonObject ();
+		msg ["_rpcId"] = rpcId;
+		msg ["cmd"] = "ok";
+		HelloWorld.pclient.request(
+			"connector.gameHandler.rpcResponse", 
+			msg, 
+			(JsonObject dummy) => {}
+		);
+		yield return 0;
+
+		dataJson = rpcResponse[rpcId];
+		DevLog(SimpleJson.SimpleJson.SerializeObject(dataJson));
 	}
 }
